c382847081d0fc710826e9d63c84f4b4c3efba77
McGrg <mcgrg@yandex.ru>
2019-04-24 23:19:57 +0300

    1. Lets start

---
+++ AosrSber/Cement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AosrSber
+{
+    class Cement
+    {
+        public string Date { get; set; }
+        public string DrillNumber { get; set; }
+        public string HoleNumber { get; set; }
+        public string HoleLenght { get; set; }
+        public string StartLevel { get; set; }
+        public string FinishLevel { get; set; }
+        public string Angle { get; set; }
+        public string Volume { get; set; }
+        public string Pressure { get; set; }
+        public string ConcreteDate { get; set; }
+        public string ConcreteNumber { get; set; }
+
+        public Cement(string Date, string DrillNumber, string HoleNumber, string HoleLenght, string StartLevel, string FinishLevel, string Angle, string Volume, string Pressure, string ConcreteDate, string ConcreteNumber)
+        {
+            this.Date = Date;
+            this.DrillNumber = DrillNumber;
+            this.HoleNumber = HoleNumber;
+            this.HoleLenght = HoleLenght;
+            this.StartLevel = StartLevel;
+            this.FinishLevel = FinishLevel;
+            this.Angle = Angle;
+            this.Volume = Volume;
+            this.Pressure = Pressure;
+            this.ConcreteDate = ConcreteDate;
+            this.ConcreteNumber = ConcreteNumber;
+        }
+    }
+}
--- AosrSber/MainWindow.xaml.cs
+++ AosrSber/MainWindow.xaml.cs
@@ -1,28 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections;
+using Win = System.Windows;
+using Win32 = Microsoft.Win32;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.Generic;
+using Forms = System.Windows.Forms;
 
 namespace AosrSber
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Win.Window
     {
+        private string filenameSource; //расположение файла источника для полей формы
+        private List<Cement> dataFromFile = new List<Cement>();
+        Excel.Application appSource = null;
+        Excel.Workbook wBookSource = null;
+        Excel.Worksheet wSheetSource = null;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileBtn_Click(object sender, Win.RoutedEventArgs e)
+        {
+            Win32.OpenFileDialog opf = new Win32.OpenFileDialog();
+            opf.Filter = "Файлы Excel(*.xls;*.xlsx)|*.xls;*.xlsx";
+            opf.ShowDialog();
+            filenameSource = opf.FileName;
+            try
+            {
+                appSource = new Excel.Application();
+                wBookSource = appSource.Workbooks.Open(filenameSource);
+                wSheetSource = (Excel.Worksheet)wBookSource.Sheets[1];
+            }
+            catch (Exception ex)
+            {
+                Win.MessageBox.Show(ex.Message.ToString(), "An error occured in opening application file: ");
+            }
+            try
+            {
+                //downloading data from file to list of entities, add sertificate data
+            }
+            catch (Exception ex)
+            {
+                Win.MessageBox.Show(ex.Message.ToString(), "An error occured in getting data from file: ");
+            }
         }
     }
 }
